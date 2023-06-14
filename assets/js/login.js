491b7edd2a4236b7295f0aa99010e5b2260c673e
guiachavezpd <guia.chavez@peakdesign.com>
2023-06-13 21:01:19 -0700

    update login page

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,25 +4,6 @@ import { app } from './config.js'
 // Initialize Authentication
 const auth = getAuth(app)
 
-
-const signupForm = document.querySelector('.signup')
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log('signup')
-    const email = signupForm.email.value
-    const pw = signupForm.password.value
-
-    createUserWithEmailAndPassword(auth, email, pw)
-        .then((cred) => {
-            console.log('user created: ', cred.user)
-            signupForm.reset()
-        })
-        .catch((err) => {
-            console.log(err.message)
-            //add frontend display here for error message
-        })
-})
-
 // login and logout
 const logoutButton = document.querySelector('.logout')
 logoutButton.addEventListener('click', () => {
@@ -45,6 +26,7 @@ loginButton.addEventListener('submit', (e) => {
     signInWithEmailAndPassword(auth, email, pw)
         .then((cred) => {
             console.log('user logged in: ', cred.user)
+            window.location.href ="./../main/ownerpost.html"
         })
         .catch((err) => {
             console.log(err.message)
