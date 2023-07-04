1dd2c1a1ad5e939c9b3fa92f22ff8c70ccfad785
guiachavezpd <guia.chavez@peakdesign.com>
2023-07-03 22:11:17 -0700

    comment out the map for now

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,79 +1,71 @@
 import { tomtomAPI } from './config.js'
-// const locationQuery = localStorage.getItem('location-query')
-
-// if(locationQuery != null || locationQuery != '') {
-//     document.getElementById('query').value = locationQuery
-// }
 
 const homeSearch = document.getElementById('submit')
 
 const search = (e) => {
     e.preventDefault()
-    //let location = document.getElementById('query').value
     let type = document.getElementById('type').value
     let source = document.getElementById('source').value
 
     
     localStorage.setItem('type', type)
     localStorage.setItem('source', source)
-    
-    //locationSearch(location)
-    
-    
 
-    
-
+    // redirect to animals page
     setTimeout(function() {
         window.location.href = 'http://127.0.0.1:5500/main/animals.html';
     }, 3000)
 }
 
-let mapContainer = document.getElementById('map')
-let marker
+// COMMENT OUT THE MAP
+// let mapContainer = document.getElementById('map')
+// let marker
 
-var map = tt.map({
-    key: tomtomAPI,
-    container: mapContainer,
-    center:  [-123.120735, 49.282730],
-    zoom: 14
-})
+// var map = tt.map({
+//     key: tomtomAPI,
+//     container: mapContainer,
+//     center:  [-123.120735, 49.282730],
+//     zoom: 14
+// })
 
 // searching
-var locationSearch = (setLocation) => {
-    console.log(setLocation);
+// var locationSearch = (setLocation) => {
+//     console.log(setLocation);
 
-    tt.services.fuzzySearch({
-      key: tomtomAPI,
-      query: setLocation,
-      //boundingBox: map.getBounds()
+//     tt.services.fuzzySearch({
+//       key: tomtomAPI,
+//       query: setLocation,
+//       //boundingBox: map.getBounds()
 
-    }).then(handleResult)
+//     }).then(handleResult)
   
-    localStorage.setItem('location-query', setLocation)
+//     localStorage.setItem('location-query', setLocation)
   
-}
+// }
 
-var handleResult = function(response) {
-    console.log(response)
-    if(response.results) {
-        let position = response.results[0].position
-        moveMap(position)
-        localStorage.setItem('position', JSON.stringify(position))
+// var handleResult = function(response) {
+//     console.log(response)
+//     if(response.results) {
+//         let position = response.results[0].position
+//         moveMap(position)
+//         localStorage.setItem('position', JSON.stringify(position))
 
-        marker = new tt.Marker()
-                    .setLngLat(position)
-                    .addTo(map)
-    }
-}
+//         marker = new tt.Marker()
+//                     .setLngLat(position)
+//                     .addTo(map)
+//     }
+// }
 
 
-var moveMap = function(lnglat) {
-    map.flyTo({
-        center: lnglat,
-        zoom: 14
-    })
-}
+// var moveMap = function(lnglat) {
+//     map.flyTo({
+//         center: lnglat,
+//         zoom: 14
+//     })
+// }
 
+
+// FUNCTION FOR LOCATION SEARCH
 var searchOptions = {
     key: tomtomAPI,
     language: 'en-Gb',
@@ -102,72 +94,7 @@ ttSearchBox.on('tomtom.searchbox.resultselected', function(event) {
     localStorage.setItem('position', JSON.stringify(event.data.result.position))
 })
 
-// var state = {
-//     previousOptions: {
-//         query: null,
-//         center: null
-//     },
-//     callbackId: null,
-//     userLocation: null
-// };
 
-// map.addControl(new tt.FullscreenControl({container: document.querySelector('body')}));
-// map.addControl(new tt.NavigationControl());
-
-
-// var geolocateControl = new tt.GeolocateControl({
-//     positionOptions: {
-//         enableHighAccuracy: false
-//     }
-// });
-
-// geolocateControl.on('geolocate', function(event) {
-//     var coordinates = event.coords;
-//     state.userLocation = [coordinates.longitude, coordinates.latitude];
-//     ttSearchBox.updateOptions(Object.assign({}, ttSearchBox.getOptions(), {
-//         distanceFromPoint: state.userLocation
-//     }));
-// });
-
-// map.addControl(geolocateControl);
-// map.on('moveend', handleMapEvent);
-
-// function handleMapEvent() {
-//     // Update search options to provide geobiasing based on current map center
-//     var oldSearchOptions = ttSearchBox.getOptions().searchOptions;
-//     var oldautocompleteOptions = ttSearchBox.getOptions().autocompleteOptions;
-//     var newSearchOptions = Object.assign({}, oldSearchOptions, { center: map.getCenter() });
-//     var newAutocompleteOptions = Object.assign({}, oldautocompleteOptions, { center: map.getCenter() });
-//     ttSearchBox.updateOptions(Object.assign({}, searchBoxOptions, {
-//         placeholder: 'Query e.g. Washington',
-//         searchOptions: newSearchOptions,
-//         autocompleteOptions: newAutocompleteOptions,
-//         distanceFromPoint: state.userLocation
-//     }));
-//     console.log(ttSearchBox.getOptions())
-// }
-
-
-
-// var updateSearchOptions = function() {
-//     let options = tt.SearchBox.getOptions()
-//     options.searchOptions.boundingBox = map.getBounds()
-//     ttSearchBox.updateOptions(options)
-//     console.loh(options)
-// }
-
-// map.on('dragend', function() {
-//     updateSearchOptions()
-// })
 $(document).ready(function() {
     homeSearch.addEventListener('click', search);
-
-    // let location = document.getElementById('query').value
-    // let type = document.getElementById('type').value
-    // let source = document.getElementById('source').value
-
-    // if(location == '' || type == '' || source == '') {
-    //     document.getElementById('submit').disabled = true; 
-    // }
-
 })
