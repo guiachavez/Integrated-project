02575152859ee1927905aec4ea7aedcf20b1c188
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2023-07-03 18:18:30 -0700

    Updated Tomtom API to the config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,15 +1,15 @@
 // Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
-  
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAdLOXJVUxHlv8GCi6G_0j-12e_cCmzGkw",
-    authDomain: "fir-projects-37dfd.firebaseapp.com",
-    projectId: "fir-projects-37dfd",
-    storageBucket: "fir-projects-37dfd.appspot.com",
-    messagingSenderId: "552149961611",
-    appId: "1:552149961611:web:c54ea11939e6da05d57d8e",
-    measurementId: "G-FKNKXH7SJW"
+  apiKey: "AIzaSyAdLOXJVUxHlv8GCi6G_0j-12e_cCmzGkw",
+  authDomain: "fir-projects-37dfd.firebaseapp.com",
+  projectId: "fir-projects-37dfd",
+  storageBucket: "fir-projects-37dfd.appspot.com",
+  messagingSenderId: "552149961611",
+  appId: "1:552149961611:web:c54ea11939e6da05d57d8e",
+  measurementId: "G-FKNKXH7SJW",
 };
 
 // Initialize Firebase
@@ -19,9 +19,11 @@ const analytics = getAnalytics(app);
 export { app };
 
 // Petfinder
-const petfinderAPI = "7NJvkG519gKR29OuC2pPJT8B3lcK3hYLeTlgpsMMSj31b3dS29"
-const token = "xyCskdF4PT4wSsvBXXlMdsqrAe3HWuLayJpXmfSn"
+const petfinderAPI = "7NJvkG519gKR29OuC2pPJT8B3lcK3hYLeTlgpsMMSj31b3dS29";
+const token = "xyCskdF4PT4wSsvBXXlMdsqrAe3HWuLayJpXmfSn";
 
-export { petfinderAPI, token }
+export { petfinderAPI, token };
 // Tomtom API
- 
+const tomtomAPI = "4NGblFt1cWFjRxqAPtg7qW4jUfUYjzS1";
+
+export { tomtomAPI };
