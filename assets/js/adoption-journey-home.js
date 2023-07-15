a5a644082a55f8d7d5e8298dae40f1cbc59112b1
guiachavezpd <guia.chavez@peakdesign.com>
2023-07-15 02:23:49 -0700

    update logic

--- assets/js/adoption-journey-home.js
+++ assets/js/adoption-journey-home.js
@@ -9,44 +9,79 @@ const db = getFirestore(app)
 const adoptJourney = collection(db, 'adoptionJourney')
 const aniRef = collection(db, "animals")
 
+let adposts = []
 getDocs(adoptJourney).then((posts) => { 
-    let adposts = []
+    
  
     posts.docs.forEach( (doc) => {
-        adposts.push({...doc.data(), id: doc.id })
-
-        let postsdata = doc.data()
-
-        const petPhoto = query(aniRef, where(documentId(), "==", postsdata.petDetails.petId))
-
-        getDocs(petPhoto).then((photo) => {
-        
-            let petphoto = []
-            console.log(petphoto)
-
-            photo.docs.forEach( (doc) => {
-                petphoto.push({...doc.data(), id: doc.id })
-                let petphotodata = doc.data()
-
-                let photo = petphotodata.photo[0];
-                let table = document.getElementById('postDetails');
-
-                // document.querySelector('#petphoto').src = photo;
-
-                let row  = `<tr>
-                    <td>${postsdata.title}</td>
-                    <td>${postsdata.body}</td>
-                    <td>${petphotodata.photo[0]}</td>
-                    <td>${postsdata.petDetails.petName}</td>
-                    <td>${postsdata.authorDetails.firstName}</td>
-                    <td>${postsdata.petDetails.ownerLname}</td>
-                    <td>${postsdata.authorDetails.city}</td>
-                    <td>${postsdata.authorDetails.state}</td>
-                </tr>`;
-  
-                table.innerHTML += row
-
-            })
-        })
+        adposts.push([doc.id, {...doc.data()}])
     })
+
+    for(const i in adposts) {
+        let petPhoto = adposts[i][1].photo
+        console.log(adposts[i][1].body)
+        $('#stories').append([
+            $('<div />', {'class': `story story-${i}`, 'data-id': `${adposts[i][0]}`}).append([
+                $('<div />', {'class': 'story-details'}).append([
+                    $('<div />', {'class': 'story-photos slider'}),
+                    $('<div />').append([
+                        $('<p />', {text: `${adposts[i][1].petDetails.petName}`, class: 'pet-name'}),
+                        $('<p />', {text: 'Adopted by ', class: 'owner-name'}).append([
+                            $('<span />', {text: `${adposts[i][1].petDetails.ownerFname}`})
+                        ]),
+                        $('<p />', {text: `${adposts[i][1].authorDetails.city}, ${adposts[i][1].authorDetails.state}`, class: 'owner-location'})
+                    ])
+                ]),
+                $('<div />', {'class': 'story-content'}).append([
+                    $('<p />', {text: `${adposts[i][1].title}`, class: 'story-title'}),
+                    $('<p />', {text: `${adposts[i][1].body}` })
+                ])
+            ])
+        ])
+
+        for(const key in petPhoto) {
+            $(`.story-${i} .story-photos`).append([
+                $('<div />', {'class': `story-img`}).append([
+                    $('<img>', {'src': petPhoto[key]})
+                ])
+            ])
+        }
+
+        $('.story-photos').not('.slick-initialized').slick({
+            infinite: true,
+            dots: true,
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+    }
 })
+
+$(document).ready(function() {  
+    var article = document.getElementsByClassName('article');
+    
+    for (let i = 0; i < article.length; i++) {
+        article[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+
+            if ($(this).hasClass("active")) {
+                $(this).find('img').css('transform', 'rotate(90deg)')
+            } else {
+                $(this).find('img').css('transform', 'rotate(0deg)')
+            }
+
+            var content = this.nextElementSibling;
+
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            } 
+        });
+    }
+});
+
+
+
+
+
