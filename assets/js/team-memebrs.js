683b8623d9280c818afc3b5bb9db84418f9c3ccd
sim0511 <simrandeep2012@gmail.com>
2023-07-22 21:11:26 -0700

    team members page

---
+++ assets/js/team-memebrs.js
@@ -0,0 +1,12 @@
+$('.image-container').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    arrows: true,
+    fade: true,
+    useTransform: false,
+    prevArrow:'<button class="slick-prev">&larr;</button>',
+    nextArrow:'<button class="slick-next">&rarr;</button>'
+});
+                  
